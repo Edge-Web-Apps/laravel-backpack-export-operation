17a055b22376a89749488cd5165fe55a71995387
Bri MacBain <35091265+brimacbain@users.noreply.github.com>
2024-10-30 11:28:34 -0700

    Update CrudExport.php

--- src/Exports/CrudExport.php
+++ src/Exports/CrudExport.php
@@ -32,22 +32,23 @@ class CrudExport implements FromView, ShouldAutoSize
         $log_model = config('backpack.operations.export.export_log_model');
         $log = $log_model::find($this->export_log->id);
 
-        $parts = parse_url($this->export_log->export_parameters['_http_referrer']);
-        parse_str($parts['query'], $query);
+//        $parts = parse_url($this->export_log->export_parameters['_http_referrer']);
+//        parse_str($parts['query'], $query);
+        $query = $this->export_log->export_parameters;
 
         CRUD::setModel($log->model);
 
         //Accept parameters
-        if(isset($query['from_to']) && !isset($query['status1campaign'])){
+        if(isset($query['from_to']) && !isset($query['rollover_campaign'])){
             $query['from_to'] = json_decode($query['from_to']);
             $entries = $log->model::whereBetween('created_at', [$query['from_to']->from, $query['from_to']->to])->get();
         }
-        elseif(!isset($query['from_to']) && isset($query['status1campaign'])){
-            $entries = $log->model::where('rollover_campaign',$query['status1campaign'])->get();
+        elseif(!isset($query['from_to']) && isset($query['rollover_campaign'])){
+            $entries = $log->model::where('rollover_campaign',$query['rollover_campaign'])->get();
         }
-        elseif(isset($query['from_to']) && isset($query['status1campaign'])){
+        elseif(isset($query['from_to']) && isset($query['rollover_campaign'])){
             $query['from_to'] = json_decode($query['from_to']);
-            $entries = $log->model::whereBetween('created_at', [$query['from_to']->from, $query['from_to']->to])->where('rollover_campaign',$query['status1campaign'])->get();
+            $entries = $log->model::whereBetween('created_at', [$query['from_to']->from, $query['from_to']->to])->where('rollover_campaign',$query['rollover_campaign'])->get();
         }
         else{
             $entries = $log->model::all();
